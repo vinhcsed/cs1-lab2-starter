39e8b4795a95657bf2809e7c0172f154e3a65c5a
natalie403 <natvu314@gmail.com>
2025-03-04 20:36:57 -0800

    ID, Pseudocode, Main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+float x = 3.00;
+float y = 15.73;
+float z = x + y;
+
+float a, b;
+float c = a + b;
+
+int main() {
+
+// Part 1: Vertical Output
+
+    cout << fixed
+         << setprecision(2)
+         << setw(7);
+
+    cout << x << endl;
+    cout << "+ " << y << endl;
+    cout << "-------" << endl;
+    cout << "  " << z << endl;
+
+// Part 2: Interactive Input
+
+    cout << fixed
+         << setprecision(2)
+         << setw(7);
+
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b; 
+
+    cout << "  " <<  a << endl;
+    cout << "+ " << b << endl;
+    cout << "-------" << endl;
+    cout << " " << c << endl;
+
+    return 0;
+
+}
